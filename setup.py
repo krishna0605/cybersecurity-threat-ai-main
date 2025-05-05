b03f67ea4dec07a44e3d647aa469eaea9fff0a4f
krishna kapoor <creativesimulation1@gmail.com>
2025-05-05 18:10:05 +0530

    deployment 3

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="cybersecurity-threat-ai",
     version="0.1.0",
     packages=find_packages(),
+    setup_requires=[
+        "setuptools>=69.0.2",
+        "setuptools-distutils>=36.0.0"
+    ],
     install_requires=[
         "flask==2.2.3",
         "werkzeug==2.2.3",
